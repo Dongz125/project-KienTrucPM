c6e9944d956b4715f4dd39acb42d58daa2c6810c
Dongz125 <hoangkhai3122004@gmail.com>
2025-09-07 13:56:11 +0700

    add candlestick view page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,54 +1,46 @@
 import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoadingPage from "./pages/LoadingPage";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import TradingViewPage from "./pages/TradingViewPage";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [page, setPage] = useState<"login" | "register" | "home">("login");
+    const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    fetch(`${import.meta.env.VITE_API_URL}/hello`)
-      .then((res) => {
-        if (res.status === 200) {
-          return res.text();
-        }
-        return Promise.reject("API error");
-      })
-      .then((txt) => {
-        console.log(txt); // "hello world"
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.error(err);
-        setLoading(false);
-      });
-  }, []);
+    useEffect(() => {
+        fetch(`${import.meta.env.VITE_API_URL}/hello`)
+            .then((res) => {
+                if (res.status === 200) {
+                    return res.text();
+                }
+                return Promise.reject("API error");
+            })
+            .then((txt) => {
+                console.log(txt);
+                setLoading(false);
+            })
+            .catch((err) => {
+                console.error(err);
+                setLoading(false);
+            });
+    }, []);
 
-  if (loading) {
-    return <LoadingPage />;
-  }
+    if (loading) {
+        return <LoadingPage />;
+    }
 
-  return (
-    <>
-      {page === "login" && (
-        <LoginPage
-          onLoginSuccess={() => setPage("home")}
-          onGoRegister={() => setPage("register")}
-        />
-      )}
-
-      {page === "register" && (
-        <RegisterPage
-          onRegisterSuccess={() => setPage("home")}
-          onGoLogin={() => setPage("login")}
-        />
-      )}
-
-      {page === "home" && <HomePage />}
-    </>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/trading" element={<TradingViewPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
